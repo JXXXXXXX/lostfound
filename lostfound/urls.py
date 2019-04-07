441fbc3371b61f5001c9b64ce00b608b57559907
Jin <jinxin19980410@163.com>
2019-04-07 11:00:15 +0800

    美化二级页面

--- lostfound/urls.py
+++ lostfound/urls.py
@@ -21,16 +21,16 @@ from model.views import *
 
 urlpatterns = [
     url('admin/', admin.site.urls), # django管理员界面
-    url(r'^login$',login_view),     # 登陆界面(新)
-    url(r'^upload$',upload_view),  # 物品信息上传界面（新）
-    #物品id 20190325102706934929
+    url(r'^login$',login_view),     # 登陆界面
+    url(r'^upload$',upload_view),  # 物品信息上传界面
     url(r'^object/(?P<object_id>[0-9]{20})$',objShowinfo_view,name='object'), # 物品信息显示页面
-    url(r'^object$',objList_view), # 二级界面
     url(r'^profile/(?P<nav_id>[0-2]{1})$',profile_view,name='profile'), # 个人中心-用户
     url(r'^quit$',quit_view), # 退出按钮
     url(r'^main$',main_view), # 主界面
+    url(r'^$',main_view),
     url(r'^sort/(?P<sort_id>[0-6]{1})$',sort_view,name='sort'), # 物品分类显示
     url(r'^complete_info$',complete_view),# 修改密码并完善个人信息
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # "+static"是显示图片的需要（参考https://blog.csdn.net/c_beautiful/article/details/79755368
 
